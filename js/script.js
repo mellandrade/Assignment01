48c913e331b189898a6e10ae532647308f3709ce
mellandrade <30361727+mellandrade@users.noreply.github.com>
2019-02-07 21:25:00 -0800

    FEWD 2 - Assignment 1
    Almost complelte.

---
+++ js/script.js
@@ -0,0 +1,202 @@
+/* eslint-env browser*/
+
+// STEP 1
+/*
+var some_Month;
+function the Month();
+var current-Month;
+var summer_Month;
+var mylibrary-Function
+*/
+
+// STEP 2
+/*
+81.8                //numeric 
+"My name is Mel!"   //string
+true                //Boolean
+null                //null
+*/
+
+// STEP 3
+/*
+var complexExample = 67 + (3/6) - 8;
+var anotherComplex = (base * height)/2;
+*/
+
+// STEP 4
+/*
+var firstName;           //First Name
+var lastName;            //Last Name
+var txtAddress;          //Address
+var txtCity;             //City
+var txtState;            //State
+var numZipCode;          //Zip Code
+var numAge;              //Your Age
+var txtReferralSource;   //Referral Source
+var txtContact;          //May We Contact You
+*/
+
+// STEP 5
+/*
+var firstName = "Mellisa";           
+var lastName = "Andrade";            
+
+var txtAddress;         
+var txtCity;            
+var txtState;            
+var numZipCode;
+txtAddress = "135 Red North";
+txtCity = "Boston";
+txtState = "Massachusetts";
+numZipCode = "02130";
+
+var numAge = "35";              
+var txtReferralSource; 
+var txtContact = "May We Contact You", txtReferralSource = "Magazine";  
+*/
+
+// STEP 6
+/*
+var booksRead = 100;
+window.console.log("Mell read over " + booksRead + " books last year.");
+*/
+
+// STEP 7   REVIEW
+/*
+var x = 40;
+var y = 35;
+window.console.log(x==y);
+
+y += 5;
+window.console.log(y==40);
+*/
+
+// STEP 8
+/*
+var someString = "Who once said, Only two things are infinite, the universe and human stupidity, and I'm not sure about the former.";
+window.console.log(someString);
+*/
+
+// STEP 9
+/*
+var x = null;
+window.console.log(x);
+
+var y;
+window.console.log(y);
+*/
+
+// STEP 10  REVIEW
+/*
+typeof "Hey" + "<br>" +
+typeof 8 + "<br>" +
+typeof true + "<br>" +
+typeof [2,4,6,8] + "<br>" +
+typeof myBook + "<br>";
+window.console.log(typeof);
+*/
+
+// STEP 11
+/*
+var fullName = "Mell Andrade";
+var className = "JavaScript";
+alert("Hello " + fullName + ", welcome to the " + className + " class!");
+*/
+
+// STEP 12
+/*
+var name = "Mellisa Andrade";
+var className = "JavaScript";
+alert("Hello " + name + ", welcome to the " + className + " class!");
+*/
+
+// STEP 13
+/*
+var name = "Mellisa Andrade";
+var course = "JavaScript";
+alert("Hello " + name + ", welcome to the " + course + " class!");
+*/
+
+// STEP 14
+/*
+var name = "Mellisa Andrade";
+var course = "JavaScript";
+alert("Hello " + name + ". \nWelcome to the " + course + " class!");
+*/
+
+// STEP 15
+/*
+var name = prompt("What is your name?");
+var course = "JavaScript";
+alert("Hello " + name + ". \nWelcome to the " + course + " class!");
+*/
+
+// STEP 16
+/*
+var name = prompt("What is your name?");
+var course = prompt("What class are you taking?");
+alert("Hello " + name + ". \nWelcome to the " + course + " class!");
+*/
+
+// STEP 17
+/*
+var x = 10;
+var y = 20;
+var z = x + y;
+window.console.log(z);
+*/
+
+// STEP 18
+/*
+var x = 20;
+x += 20;
+window.console.log(x);
+*/
+
+// STEP 19
+/*
+var x = 20;
+x *= 5;
+window.console.log(x);
+*/
+
+// STEP 20
+/*
+var y = 20;
+var z = 3;
+var x = y % z;
+x /= 1;
+window.console.log(x);
+*/
+
+// STEP 21
+/*
+var x = 10;
+var y = 8;
+window.console.log(x > 2 && y > 1);
+*/
+
+// STEP 22
+/*
+var x = 50;
+var y = 25;
+window.console.log(x==2 || y==5);
+*/
+
+// STEP 23
+/*
+var widget = new Object();
+window.console.log(typeof widget);
+*/
+
+// STEP 24
+/*
+var widget = new Object();
+window.console.log(widget instanceof Object);
+*/
+
+// STEP 25
+/*
+var widget = new Object();
+window.console.log(widget instanceof Number);
+*/
